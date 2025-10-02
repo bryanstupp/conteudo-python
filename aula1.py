1cdb1a291396a84fea7cf883c60f441049128f52
NILSON STUPP <nilson_stupp@estudante.sesisenai.org.br>
2025-10-02 16:48:51 -0300

    conteudo sobre print e variaveis

---
+++ aula1.py
@@ -0,0 +1,10 @@
+# Como nosso primeiro código, vamos escapar da maldição
+print("Olá mundo!")
+print("Lógica","de","programação",sep=" ",end="\n")
+print("Vai","imprimir","em","baixo",sep="\n")
+#baixo cada comando possui o end alterado para imprimir na mesma linha
+print("Vai",end=" ")
+print("imprimir",end=" ")
+print("na",end=" ")
+print("mesma",end=" ")
+print("linha",end=" ")
---
+++ aula1_variaveis.py
@@ -0,0 +1,12 @@
+#Variaveis são espaços na memória que servem para armazenar valores
+
+# Podemos definir variéveis com o nome dela, seguido pelo
+# sinal de igual,que siginifica "atribuir" e deppois pelo valor.
+# nome = "Capita" <- Crie uma variavel nome que recebe o valor "Capita"
+
+nome = "Bryan"
+salario = 1000000.00
+dias_trabalhados = 1
+print("O funcionário é",nome)
+print("Ele trabalhou",dias_trabalhados,"dias")
+print("O salário é",salario)
