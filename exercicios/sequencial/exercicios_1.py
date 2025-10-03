3aa4016944a785030c23c043c4a85a9c9d88e29a
NILSON STUPP <nilson_stupp@estudante.sesisenai.org.br>
2025-10-03 16:56:12 -0300

    exercicio feito ate o 3

---
+++ exercicios/sequencial/exercicios_1.py
@@ -0,0 +1,19 @@
+print("hello world!")
+
+nome = input("digite o nome:")
+print("Olá",nome)
+
+numero_1 = int(input("digite o numero 1: "))
+numero_2 = int(input("digite o numero 2: "))
+
+
+resultado = numero_1 + numero_2
+print("A soma do número 1 com o número 2 é:",resultado)
+
+nota_1 = float(input("digite a nota 1: "))
+nota_2 = float(input("digite a nota 2: "))
+nota_3 = float(input("digite a nota 3: "))
+nota_4 = float(input("digite a nota 4: "))
+
+soma = nota_1 + nota_2 + nota_3 + nota_4
+media =
